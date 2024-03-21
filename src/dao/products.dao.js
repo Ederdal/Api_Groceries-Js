bb71e67a4e08f29c7d947e5aec5111b0227efcc4
Ederdal <degarcruz2004@gmail.com>
2024-03-21 08:43:36 -0600

    upd: Corrigiendo el error de delete del archivo puducts.dao.js

--- src/dao/products.dao.js
+++ src/dao/products.dao.js
@@ -1,39 +1,39 @@
 import Product from '../models/products.model.js'
 
-const productDAO={
-}
+const productDAO = {};
 
-productDAO.getAll=async()=>{
-    const products=await Product.find();
+productDAO.getAll = async () => {
+    const products = await Product.find();
     return products;
 };
-productDAO.getOne=async(barcode)=>{
-    const product=await Product.findOne({barcode:barcode});
-    return product;
-}
 
-productDAO.insertOne=async(product)=>{
+productDAO.getOne = async (barcode) => {
+    const product = await Product.findOne({ barcode: barcode });
+    return product;
+};
+
+productDAO.insertOne = async (product) => {
     return await Product.create(product);
-}
-
-productDAO.deleteOne=async(barcode)=>{
-    const product=await Product.findOne({barcode:barcode});
-    return product;
-}
+};
 
 productDAO.deleteOne = async (barcode) => {
     try {
-        const result = await Product.deleteOne({ barcode: barcode });
-        if(barcode)
+        const product = await Product.findOne({ barcode: barcode });
+        if (!product) {
+            // Si el producto no se encuentra en la base de datos
+            return null;
+        }
+        await product.remove(); // Elimina el producto de la base de datos
         // Producto eliminado con éxito
         return { message: 'Product deleted successfully' };
     } catch (error) {
-        return { error: error.message };
+        // Error al intentar eliminar el producto
+        throw new Error('Failed to delete product');
     }
 };
 
-productDAO.updateOne=async(barcode, product)=>{
-    return await Product.findOneAndUpdate({barcode:barcode},product); 
+productDAO.updateOne = async (barcode, product) => {
+    return await Product.findOneAndUpdate({ barcode: barcode }, product);
 };
 
 export default productDAO;
